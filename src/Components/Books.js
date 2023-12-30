bf2acf8dd59c872dadf8a859ff1da1d561555419
Ashish Kumar <48367301+Ashishrock9394@users.noreply.github.com>
2023-12-30 21:30:10 +0530

    final

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -24,8 +24,14 @@ const Books=({setBookInfo,bookinfo})=>{
     return(
         <div>
             <div className='inputdata'>
+                <div><img src="./logo.png" alt='img'></img></div>
                 <input type='text' onChange={(e)=>setQuery(e.target.value)} placeholder='Search for the book you want to read it now Sherlock Holmes,Harry potter...'/>            
                 <button onClick={fectchDetails}>Search</button>
+                <div><img src="./bx_bx-book-heart.png" alt='img'></img></div>
+                <div><img src="./Vector.png" alt='img'></img></div>
+                <div><img src="./Group.png" alt='img'></img></div>
+                <div><img src="./image.png" alt='img'></img></div>
+                
                 
             </div>
         <div>
